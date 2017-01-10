b011321105dea057f95e2b3e7bfe72bbd2fe6376
Jan Vansteenkiste <jan@vstone.eu>
2017-01-10 15:02:08 +0100

    [::r11k] Install the script in the main class + create default hook directories

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,20 @@
-# == Class: r11k
+# Installs the r11k script and creates the default hooks directories.
 #
-class r11k {
-  include '::r11k::install'
+# @param install_location location where to install r11k.
+# @param default_hooks_dir default hooks directory to use for custom hooks. NOT used in cronjobs.
+class r11k (
+  Optional[Stdlib::Absolutepath] $install_location  = '/usr/local/bin/r11k',
+  Optional[Stdlib::Absolutepath] $default_hooks_dir = '/etc/r11k/hooks.d',
+){
+
+  file { $install_location:
+    ensure => 'file',
+    mode   => '0755',
+    source => 'puppet:///modules/r11k/r11k.sh',
+  }
+
+  file {['/etc/r11k','/etc/r11k/hooks.d','/etc/r11k/config']:
+    ensure => 'directory',
+    mode   => '0755',
+  }
 }
