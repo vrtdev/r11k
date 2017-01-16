a161871836fb6169954a70750345851c8a74d0c6
Jan Vansteenkiste <jan@vstone.eu>
2017-01-16 08:50:53 +0100

    [::r11k] The default hooks dir is not to be overwritten but only used. Otherwise, specify it where used (r11k::hook)

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,17 +1,20 @@
 # Installs the r11k script and creates the default hooks directories.
 #
 # @param install_location location where to install r11k.
-# @param default_hooks_dir default hooks directory to use for custom hooks. NOT used in cronjobs.
 class r11k (
   Optional[Stdlib::Absolutepath] $install_location  = '/usr/local/bin/r11k',
-  Optional[Stdlib::Absolutepath] $default_hooks_dir = '/etc/r11k/hooks.d',
 ){
+
 
   file { $install_location:
     ensure => 'file',
     mode   => '0755',
     source => 'puppet:///modules/r11k/r11k.sh',
   }
+
+  # These are the default directories used in r11k::hook. Make sure they
+  # exist to prevent a dependency / recursive directory creation mess
+  $default_hooks_dir = '/etc/r11k/hooks.d'
 
   file {['/etc/r11k','/etc/r11k/hooks.d','/etc/r11k/config']:
     ensure => 'directory',
