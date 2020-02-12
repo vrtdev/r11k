50954289925f2d2980a2ad444fafee12ab1aacea
Jan Vansteenkiste <jan@vstone.eu>
2020-02-12 12:58:13 +0100

    Allow enabling purging on the hooks directories

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,10 +3,12 @@
 # @param install_location location where to install r11k.
 # @param crons  Hash with r11k:cron definitions to create.
 # @param hooks Hash with r11k::hook definitions to create.
+# @param purge_hooks Should we purge unmanaged files from the default hook directories.
 class r11k (
   Optional[Stdlib::Absolutepath] $install_location  = '/usr/local/bin/r11k',
   Hash $crons = {},
   Hash $hooks = {},
+  Boolean $purge_hooks = false,
 ){
 
   file { $install_location:
@@ -20,13 +22,20 @@ class r11k (
   $default_hooks_dir = '/etc/r11k/hooks.d'
   $default_env_hooks_dir = '/etc/r11k/env.hooks.d'
 
-  file {['/etc/r11k', '/etc/r11k/hooks.d', '/etc/r11k/env.hooks.d']:
+  file {'/etc/r11k':
     ensure => 'directory',
     mode   => '0755',
   }
   file {'/etc/r11k/config':
     ensure => 'directory',
     mode   => '0750',
+  }
+
+  file {[$default_hooks_dir, $default_env_hooks_dir]:
+    ensure  => 'directory',
+    mode    => '0755',
+    purge   => $purge_hooks,
+    recurse => $purge_hooks,
   }
 
   $crons.each |$name, $params| {
